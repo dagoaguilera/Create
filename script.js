e09a082c16fc54c6edd0e7cc2884c23ae245dddc
Dago Aguilera <tbedagito@gmail.com>
2024-07-29 21:39:37 -0400

    Added theme switcher

--- script.js
+++ script.js
@@ -33,3 +33,27 @@ const handleOnMove = e => {
 window.onmousemove = e => handleOnMove(e);
 
 window.ontouchmove = e => handleOnMove(e.touches[0]);
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'light') {
+        toggleSwitch.checked = true;
+        document.body.classList.add('light-mode');
+    }
+}
+
+toggleSwitch.addEventListener('change', e => {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+        document.body.classList.add('light-mode');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        document.body.classList.remove('light-mode');
+    }
+});
